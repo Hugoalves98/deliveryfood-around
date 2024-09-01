071386c2b92667ad065f60ec39d7833928cf47b1
Hugoalves98 <hugorochalves@gmail.com>
2024-08-31 23:15:12 -0300

    Ajuste texto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,11 @@
-import { View, Pressable, Text } from 'react-native';
-import { Ionicons, Feather } from '@expo/vector-icons'
+import { View, Pressable, Text } from "react-native";
+import { Ionicons, Feather } from "@expo/vector-icons";
 
 export function Header() {
- return (
-   <View className="w-full flex flex-row items-center justify-between">
+  return (
+    <View className="w-full flex flex-row items-center justify-between">
       <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
-        <Ionicons name="menu" size={20} color="#121212"/>
+        <Ionicons name="menu" size={20} color="#121212" />
       </Pressable>
 
       <View className="flex flex-col items-center justify-center">
@@ -13,13 +13,13 @@ export function Header() {
 
         <View className="flex-row items-center justify-center gap-1">
           <Feather name="map-pin" size={14} color="#FF0000" />
-          <Text className="text-lg font-bold">Campo Grande</Text>
+          <Text className="text-lg font-bold">Benfica</Text>
         </View>
       </View>
 
       <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
-        <Feather name="bell" size={20} color="#121212"/>
+        <Feather name="bell" size={20} color="#121212" />
       </Pressable>
-  </View>
+    </View>
   );
 }
